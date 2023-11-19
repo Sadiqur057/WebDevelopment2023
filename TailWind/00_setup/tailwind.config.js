67b7cd7e8d4a61155aaefc9ac2788d7922757e99
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-11-19 20:24:27 +0600

    setup of tailwinds

---
+++ TailWind/00_setup/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'index.html',
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
+  // ...
+}
