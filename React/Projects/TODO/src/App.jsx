a200f19cac2a3c4121bec72606f4994bbebdacf4
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-12-04 21:04:34 +0600

    added components

--- React/Projects/TODO/src/App.jsx
+++ React/Projects/TODO/src/App.jsx
@@ -1,48 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
+import AppName from "./components/AppName";
+import AddToDo from "./components/AddToDo";
+import ToDoItem from "./components/ToDoItem";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <center className="todo-container">
-      <h1>TODO</h1>
-
-      <div className="container text-center">
-        <div className="row">
-          <div className="col-6">
-            <input type="text" name="" id="" placeholder="Enter ToDO" />
-          </div>
-          <div className="col-4">
-            <input type="date" name="" id="" />
-          </div>
-          <div className="col-2">
-            <button className="btn btn-success">Add</button>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-6">
-            <p>Complete todo</p>
-          </div>
-          <div className="col-4">
-            11/10/23
-          </div>
-          <div className="col-2">
-            <button className="btn btn-danger">Delete</button>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-6">
-            <p>Complete todo</p>
-          </div>
-          <div className="col-4">
-            11/10/23
-          </div>
-          <div className="col-2">
-            <button className="btn btn-danger">Delete</button>
-          </div>
-        </div>
+      <AppName />
+      <AddToDo />
+      <div className="items-container">
+        <ToDoItem />
+        <ToDoItem />
       </div>
     </center>
   );
