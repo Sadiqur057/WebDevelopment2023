14ae905e488424d534134c0e101e3c0a0a14437c
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-12-04 18:51:05 +0600

    creating first component

---
+++ React/02_React Component/first react component/src/App.jsx
@@ -0,0 +1,13 @@
+import Intro from "./intro";
+import { Btn } from "./testBtn";
+Intro;
+function App(){
+  return <div>
+    <h1>
+    Hello world
+  </h1>
+  <Intro></Intro>
+  <Btn></Btn>
+  </div>
+}
+export default App;
