70bbab8a963737b1f2a6c201d692b7038443ca15
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-12-05 18:29:12 +0600

    added second component testbtn

---
+++ React/02_first-react-component/src/components/TestBtn.jsx
@@ -0,0 +1,13 @@
+// Default export
+// function Btn(){
+//   return <button>Click me</button>
+// }
+// export default Btn;
+
+// Named Export
+export function Btn(){
+  return <button>Click me</button>
+}
+
+// Named Import
+// import {Btn} from './testBtn.jsx'
