38697ef9ff51b275cfe8625596428f1a67af2bba
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-12-05 18:28:33 +0600

    first react component intro

---
+++ React/02_first-react-component/src/App.jsx
@@ -0,0 +1,12 @@
+import Intro from "./components/intro";
+import { Btn } from "./components/testBtn";
+function App(){
+  return <div>
+    <h1>
+    Hello world
+  </h1>
+  <Intro></Intro>
+  <Btn></Btn>
+  </div>
+}
+export default App;
---
+++ React/02_first-react-component/src/components/Intro.jsx
@@ -0,0 +1,6 @@
+function Intro(){
+  return <h3>Explore the greatest React Project</h3>
+}
+
+// default export
+export default Intro;
